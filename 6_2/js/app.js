15940d344e03873986b637073d863c57a01bc232
wpiusa <wpiusa@gmail.com>
2017-03-10 08:09:00 -0800

    Add files via upload

---
+++ 6_2/js/app.js
@@ -0,0 +1,11 @@
+var x=10;
+var y=2;
+var z=20;
+
+if (x > y && x > z){
+	console.log(x+" is greatest number!")
+}else if ( y > x && y > z) {
+    console.log(x+" is greatest number!")
+}else if (z > x && z > y){
+	console.log(z+" is greatest number!")
+}
